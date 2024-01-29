10c80498e15713b8a87effb000aec739f5a72699
Himanshu Bhanpurkar <hbhanpurkar@gmail.com>
2024-01-29 16:24:34 +0530

    Added navigation buttons and their functionalities

--- src/components/News.js
+++ src/components/News.js
@@ -205,28 +205,59 @@ export default class News extends Component {
         console.log("This is a constructor")
         this.state = {
             articles: this.articles,
-            loading: false
+            loading: false,
+            page: 1
         }
     }
-    
+
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=f2b970b162d74d11b4ac8896432a820a"
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=f2b970b162d74d11b4ac8896432a820a&page=1&pageSize=20"
         let data = await fetch(url)
         let parsedData = await data.json()
-        this.setState({ articles: parsedData.articles })
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
+    }
+
+    handlePrevClick = async () => {
+        // console.log("previous")
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f2b970b162d74d11b4ac8896432a820a&page=${this.state.page - 1}&pageSize=20`
+        let data = await fetch(url)
+        let parsedData = await data.json()
+
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+    }
+
+    handleNextClick = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        } else {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f2b970b162d74d11b4ac8896432a820a&page=${this.state.page + 1}&pageSize=20`
+            let data = await fetch(url)
+            let parsedData = await data.json()
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+        }
     }
 
     render() {
         return (
             <div className='container my-4'>
-                <h2>News Junkie top headlines</h2>
+                <h1>News Junkie top headlines</h1>
                 <div className='row'>
                     {this.state.articles.map((element) => {
-                      return  <div className="col-md-3" key={element.url}>
-                            <NewsItem  title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage?element.urlToImage:"https://www.nydailynews.com/wp-content/uploads/2024/01/Orla-Baxendale-2.jpg?w=1024&h=731"} newsUrl = {element.url}/>
+                        return <div className="col-md-3" key={element.url}>
+                            <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage ? element.urlToImage : "https://www.nydailynews.com/wp-content/uploads/2024/01/Orla-Baxendale-2.jpg?w=1024&h=731"} newsUrl={element.url} />
 
                         </div>
                     })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button " className="btn btn-primary" onClick={this.handlePrevClick}> &larr; Previous </button>
+                    <button disabled={this.state.page >= Math.ceil(this.state.totalResults/20)} type="button" className="btn btn-primary" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
         )
